914e88fc8cbfad4176df8f54695fbd5ccad33105
maxwell Mullin <mullinmax@gmail.com>
2018-09-02 09:21:14 -0400

    working but brake light flickers

--- signal/signal.ino
+++ signal/signal.ino
@@ -6,13 +6,13 @@
 #define PIN            4
 
 // How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS      9
+#define NUMPIXELS      10
 
 // turn signal cycle in milliseconds
-#define TURNCYCLE 800
+#define TURNCYCLE 750
 
 // brightness of turn signal
-#define BRIGHTNESS 200
+#define BRIGHTNESS 175
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
@@ -20,14 +20,53 @@ void turnSignal(int t, boolean right);
 void brakeSignal(int brightness);
 void clearSignal();
 
+boolean leftPressed = false;
+boolean rightPressed = false;
+boolean turning = false;
+boolean right = false;
+boolean braking = false;
+
 void setup() {
   pixels.begin(); // This initializes the NeoPixel library.
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(0, 255, 0));
+    pixels.show();
+    delay(250);
+  }
 }
 
 void loop() {
-  boolean turning = true;
-  boolean braking = true;
-  boolean right = true;
+
+  if (digitalRead(3) == HIGH) {
+    braking = false;
+  } else {
+    braking = true;
+  }
+
+  if (digitalRead(0) == HIGH) {
+    leftPressed = true;
+  }
+
+  if (digitalRead(1) == HIGH) {
+    rightPressed = true;
+  }
+
+  if (digitalRead(0) == LOW && digitalRead(1) == LOW) {
+    leftPressed = false;
+    rightPressed = false;
+  }
+  if (leftPressed && rightPressed) {
+    turning  = false;
+  }
+
+  if (!leftPressed && rightPressed) {
+    right = true;
+    turning = true;
+  } else if (leftPressed && ! rightPressed) {
+    right = false;
+    turning = true;
+  }
+
   clearSignal();
   if (braking) {
     brakeSignal(BRIGHTNESS);
@@ -39,7 +78,7 @@ void loop() {
 }
 
 void turnSignal(int t, boolean right) {
-  if ((millis() / t) % 2 == 0) {
+  if ((millis() % t) > (t / 2)) {
     for (int i = 0; i < NUMPIXELS; i++) {
       if ((i < NUMPIXELS / 2) ^ right) {
         pixels.setPixelColor(i, pixels.Color(239, 123, 0));
